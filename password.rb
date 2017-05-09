4d24b940c5c2c68902ffa433215d3f3ef5a6e68c
Eeeeevgen <eeeeevgen@gmail.com>
2017-05-09 11:51:30 +0300

    style corrections

--- password.rb
+++ password.rb
@@ -4,19 +4,19 @@ def passgen (len)
   consonant = %w{b c d f g h j k l m n p q r s t v w x z}
   vowel = %w{a e i o u y}
 
-  out = String.new()
+  out = String.new
 
   len.times do
     |x|
     if x % 2 == 0
-      num = (Random.rand()*consonant.length).to_i
+      num = (Random.rand*consonant.length).to_i
       letter = consonant[num]
     else
-      num = (Random.rand()*vowel.length).to_i
+      num = (Random.rand*vowel.length).to_i
       letter = vowel[num]
     end
 
-    if Random.rand().round == 1
+    if Random.rand.round == 1
       letter.upcase!
     end
     out += letter
@@ -24,4 +24,4 @@ def passgen (len)
   return out
 end
 
-puts passgen(8)
+puts passgen(ARGV[0].to_i)

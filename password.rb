3f0bb4d3447bb42fc9f5e0295c0c9da7b0b10a16
Eeeeevgen <eeeeevgen@gmail.com>
2017-05-12 14:51:18 +0300

    argument check added

--- password.rb
+++ password.rb
@@ -1,27 +1,31 @@
 # coding:  utf-8
 
 def passgen (len)
-  consonant = %w{b c d f g h j k l m n p q r s t v w x z}
-  vowel = %w{a e i o u y}
+  len = len.to_i
+  if len <= 0 then
+    return 'Argument should be a positive number'
+  else
+    consonant = %w[b c d f g h j k l m n p q r s t v w x z]
+    vowel = %w[a e i o u y]
 
-  out = String.new
+    out = String.new
 
-  len.times do
-    |x|
-    if x % 2 == 0
-      num = (Random.rand*consonant.length).to_i
-      letter = consonant[num]
-    else
-      num = (Random.rand*vowel.length).to_i
-      letter = vowel[num]
+    len.times do |x|
+      if x % 2 == 0
+        num = (Random.rand*consonant.length).to_i
+        letter = consonant[num]
+      else
+        num = (Random.rand*vowel.length).to_i
+        letter = vowel[num]
+      end
+
+      if Random.rand.round == 1
+        letter.upcase!
+      end
+      out += letter
     end
-
-    if Random.rand.round == 1
-      letter.upcase!
-    end
-    out += letter
+    return out
   end
-  return out
 end
 
-puts passgen(ARGV[0].to_i)
+puts passgen(ARGV[0])
